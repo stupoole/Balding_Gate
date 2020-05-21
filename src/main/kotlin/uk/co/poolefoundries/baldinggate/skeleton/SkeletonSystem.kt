f0fadcffdfda71457c2d8c2ebca5cb88b273930b
StuPle <stuart.f.poole@gmail.com>
2020-05-21 14:35:36 +0100

    Skeletons now move towards player 1 if there is only one player
    added lines to get all player entities into "addedToEngine" in the creation of skeleton system and this allows player position to be used in SkeletonSystem.act()

--- src/main/kotlin/uk/co/poolefoundries/baldinggate/skeleton/SkeletonSystem.kt
+++ src/main/kotlin/uk/co/poolefoundries/baldinggate/skeleton/SkeletonSystem.kt
@@ -4,8 +4,11 @@ import com.badlogic.ashley.core.*
 import com.badlogic.ashley.utils.ImmutableArray
 import com.badlogic.gdx.utils.Array
 import uk.co.poolefoundries.baldinggate.PositionComponent
+import uk.co.poolefoundries.baldinggate.Resources.get
 import uk.co.poolefoundries.baldinggate.StatsComponent
 import uk.co.poolefoundries.baldinggate.WallComponent
+import uk.co.poolefoundries.baldinggate.ai.WorldState
+import uk.co.poolefoundries.baldinggate.model.PlayerComponent
 import uk.co.poolefoundries.baldinggate.model.SkeletonComponent
 import kotlin.random.Random
 
@@ -14,11 +17,20 @@ object SkeletonSystem : EntitySystem() {
 
     private var walls = ImmutableArray(Array<Entity>())
     private var skeletons = ImmutableArray(Array<Entity>())
+    private var players = ImmutableArray(Array<Entity>())
+
 
     override fun addedToEngine(engine: Engine) {
         super.addedToEngine(engine)
         walls = engine.getEntitiesFor(
             Family.all(WallComponent::class.java, PositionComponent::class.java).get()
+        )
+        players = engine.getEntitiesFor(
+            Family.all(
+                PlayerComponent::class.java,
+                PositionComponent::class.java,
+                StatsComponent::class.java
+            ).get()
         )
         skeletons = engine.getEntitiesFor(
             Family.all(
@@ -30,10 +42,19 @@ object SkeletonSystem : EntitySystem() {
     }
 
     fun act() {
+        val playerPos:PositionComponent
+        playerPos = if (players.size() == 1){
+            val player = players.get(0)
+            positionMapper.get(player)
+        } else {
+            print("multiple players found. Assuming player at 5,5")
+            PositionComponent(5,5)
+        }
+
         skeletons.forEach { skeleton ->
             val pos = positionMapper.get(skeleton)
             // TODO: Add way to get player position.
-            val newPos = SkeletonAI.getNewPosition(pos, PositionComponent(5, 5))
+            val newPos = SkeletonAI.getNewPosition(pos, playerPos)
 
             if (walls.none { positionMapper.get(it) == newPos }) {
                 skeleton.add(newPos)
